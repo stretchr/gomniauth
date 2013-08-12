3163d0dbb8afe387abf7ee063694c8f0e75e2d2f
Mat Ryer <mat@stretchr.com>
2013-08-12 15:52:42 -0600

    commented out unused thing

--- common/pre_tripper.go
+++ common/pre_tripper.go
@@ -1,8 +1,10 @@
 package common
 
+/*
 // PreTripper represents an object that gets to modify a
 // request before a round trip is made.
 type PreTripper interface {
 	// PreRoundTrip is called before the round trip is made.
 	PreRoundTrip(tripper Tripper) error
 }
+*/
