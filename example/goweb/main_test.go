64f3f1500e5781de13943fb3d4a53f2f870e511c
Mat Ryer <mat@stretchr.com>
2013-09-03 20:01:38 -0600

    added comment explaining weird test file

--- example/goweb/main_test.go
+++ example/goweb/main_test.go
@@ -4,4 +4,8 @@ import (
 	"testing"
 )
 
+// TestSomething is a place holder to make sure this package
+// gets built / compiled when running tests.
+//
+// See the notes in main.go to see the example in action.
 func TestSomething(t *testing.T) {}
